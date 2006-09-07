4586239c2e536363292ba45b3ad1f4d7f845b206
jrummell <jrummell@0d01e467-c127-4b78-8995-69daab3ef4c6>
2006-09-07 02:14:17 +0000

    Changed encrypt to MD5Encrypt and added RsaEncrypt and RsaDecrypt.

--- v2/PMTComponents/Encryption.cs
+++ v2/PMTComponents/Encryption.cs
@@ -6,16 +6,42 @@ namespace PMTComponents
 {
 	public class Encryption
 	{
+        #region MD5
         /// <summary>
-        /// Encrypt a users password
+        /// A one way encryption using an MD5 Hash
         /// </summary>
-        /// <param name="toEncrypt">Password to encrypt</param>
-        /// <returns>Encrypted password</returns>
-        public static string encrypt(string toEncrypt)
+        /// <param name="toEncrypt">string to encrypt</param>
+        /// <returns>encrypted string</returns>
+        public static string MD5Encrypt(string toEncrypt)
         {
             MD5 md5 = MD5.Create();
             string encrypted = BitConverter.ToString(md5.ComputeHash(Encoding.ASCII.GetBytes(toEncrypt))).Replace("-", String.Empty).ToLower();
             return encrypted;
         }
+        #endregion
+
+        #region RSA
+        /// <summary>
+        /// Encrypt using RSA.  Can be decrypted with RsaDecrypt.
+        /// </summary>
+        /// <param name="s">string to encrypt</param>
+        /// <returns>encrypted string</returns>
+        public static string RsaEncrypt(string s)
+        {
+            RSA rsa = RSA.Create("Rijndael");
+            return BitConverter.ToString(rsa.EncryptValue(Encoding.ASCII.GetBytes(s)));
+        }
+
+        /// <summary>
+        /// Decrypt a string encrypted with RsaEncrypt.
+        /// </summary>
+        /// <param name="s">string to decrypt</param>
+        /// <returns>decrypted string</returns>
+        public static string RsaDecrypt(string s)
+        {
+            RSA rsa = RSA.Create("Rijndael");
+            return BitConverter.ToString(rsa.EncryptValue(Encoding.ASCII.GetBytes(s)));
+        }
+        #endregion
 	}
 }
