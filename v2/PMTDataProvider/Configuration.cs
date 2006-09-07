d017beb50d5055df8dca4484afe9aef68fd3e64d
John Rummell <john@jrummell.com>
2006-09-07 02:15:50 +0000

    Added a setter to ConnectionString.  Calls SetValue, which has some issues with asp.net file security.

--- v2/PMTDataProvider/Configuration.cs
+++ v2/PMTDataProvider/Configuration.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Configuration;
+using System.Data;
+using System.Xml;
 
 namespace PMTDataProvider
 {
@@ -18,7 +20,8 @@ namespace PMTDataProvider
         /// </summary>
         public static string ConnectionString
         {
-            get {   return ConfigurationSettings.AppSettings["ConnectionString"];   }
+            get {   return GetConfigSetting("ConnectionString");    }
+            set {   SetValue("ConnectionString", value);            }
         }
 
         /// <summary>
@@ -26,7 +29,23 @@ namespace PMTDataProvider
         /// </summary>
         public static string DataProvider
         {
-            get {   return ConfigurationSettings.AppSettings["DataProvider"];   }
+            get {   return GetConfigSetting("DataProvider");   }
+        }
+
+        private static string GetConfigSetting(string setting)
+        {
+            return ConfigurationSettings.AppSettings[setting];
+        }
+
+        private static void SetValue(string key, string val)
+        {
+            string path = System.Web.HttpRuntime.AppDomainAppPath + "conn.xml";
+            DataSet ds = new DataSet();
+
+            ds.ReadXml(path);
+            DataRow[] dr = ds.Tables["add"].Select(String.Format("key='{0}'", key));
+            dr[0]["value"] = val;
+            ds.WriteXml(path);
         }
 	}
 }
